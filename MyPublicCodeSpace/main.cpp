a969af1b00c05fcb04eb2e7e6187a9baee1a112b
yangguangqian <v_yangguangqian@baidu.com>
2018-09-28 17:08:41 +0800

    修改version001

--- MyPublicCodeSpace/main.cpp
+++ MyPublicCodeSpace/main.cpp
@@ -4,7 +4,7 @@ void main()
 {
 	std::cout << "Hello Git @ github" << std::endl;
 
-	std::cout << "I am version_0_0_1 branch" << std::endl;
+	std::cout << "I am version_0_0_2 branch" << std::endl;
 
 
 
