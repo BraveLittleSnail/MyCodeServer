8fea679381f7a210d681d9b620b95abb35def88b
yangguangqian <v_yangguangqian@baidu.com>
2018-09-28 17:38:22 +0800

    I am master

--- MyPublicCodeSpace/main.cpp
+++ MyPublicCodeSpace/main.cpp
@@ -4,10 +4,6 @@ void main()
 {
 	std::cout << "Hello Git @ github" << std::endl;
 
-	std::cout << "I am version_0_0_1 branch" << std::endl;
-
-
-
-
+	std::cout << "I am master branch" << std::endl;
 
 }
