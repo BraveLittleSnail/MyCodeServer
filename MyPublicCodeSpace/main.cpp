6e3a6126d71e9332878121728b7bc3eb50320209
yangguangqian <v_yangguangqian@baidu.com>
2018-09-28 17:34:51 +0800

    在version_0_0_1添加一个函数，并在main中调用

--- MyPublicCodeSpace/main.cpp
+++ MyPublicCodeSpace/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+//在version_0_0_1在main中调用了一个函数
+int Add(int a, int b)
+{
+	return a + b;
+}
+
 void main()
 {
 	std::cout << "Hello Git @ github" << std::endl;
@@ -7,7 +13,7 @@ void main()
 	std::cout << "I am version_0_0_1 branch" << std::endl;
 
 
-
+	int temp = Add(1, 1);
 
 
 }
