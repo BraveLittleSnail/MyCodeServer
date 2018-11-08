f3cafa49614bd30a0c59369d8a9b94d194e1aa96
ygq <123>
2018-11-08 20:24:43 +0800

    add testDump

---
+++ testDump/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <assert.h>
+class A
+{
+public:
+	A()
+	{
+		m_value = 1;
+	}
+	void add()
+	{
+		std::cout << m_value << std::endl;
+	}
+protected:
+private:
+	int m_value;
+};
+
+double GenerateNaN()
+{
+	unsigned long nan[2] = { 0xffffffff, 0x7fffffff };    // code representing a NaN
+	return *(double*)nan;
+}
+void main()
+{
+	double a = GenerateNaN();
+	int b = 1;
+	int aaaa = _isnan(b);
+	assert(a!=1);
+	int zzz = 0;
+
+}
